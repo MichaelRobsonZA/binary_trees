572c3c937918d03dac8f116e0e7b63dcfddebb8d
MichaelRobsonZA <michaelrobsonza@gmail.com>
2023-08-03 18:23:47 -0700

    completed 16. Is perfect & 18. Uncle

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,57 @@
 #include "binary_trees.h"
 
 /**
+ *binary_tree_is_perfect - Checks if a binary tree is perfect.
+ *@tree: A pointer to the root node of the tree to check.
+ *
+ *Return: 1 if the tree is perfect, 0 otherwise.
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height == right_height)
+	{
+		if (binary_tree_is_full(tree) &&
+			binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right))
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
+
+/**
+ *binary_tree_height - Measures the height of a binary tree.
+ *@tree: A pointer to the root node of the tree to measure the height of.
+ *
+ *Return: The height of the tree.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	if (left_height > right_height)
+		return (left_height + 1);
+	else
+		return (right_height + 1);
+}
+
+/**
  *binary_tree_is_full - Checks if a binary tree is full.
  *@tree: A pointer to the root node of the tree to check.
  *
--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,18 +1,21 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_sibling - Finds the sibling of a binary tree node.
- *@node: A  node to find the sibling of.
+ *binary_tree_uncle - Finds the uncle of a node in a binary tree.
+ *@node: A pointer to the node to find the uncle.
  *
- *Return: A node, or NULL if node is NULL or has no sibling.
+ *Return: A pointer to the uncle node, or NULL if the uncle does not exist.
  */
-binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node == NULL || node->parent == NULL)
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
 		return (NULL);
 
-	if (node->parent->left == node)
-		return (node->parent->right);
+	binary_tree_t *parent = node->parent;
+	binary_tree_t *grandparent = parent->parent;
+
+	if (grandparent->left == parent)
+		return (grandparent->right);
 	else
-		return (node->parent->left);
+		return (grandparent->left);
 }
